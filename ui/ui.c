1d0bfd4ed7b0936152ba86d435a5335f72173ffe
IotaHydrae <writeforever@foxmail.com>
2022-09-05 01:52:45 +0800

    sync
    Signed-off-by: IotaHydrae <writeforever@foxmail.com>

--- ui/ui.c
+++ ui/ui.c
@@ -95,7 +95,7 @@ void ui_ScreenEpinkHome_screen_init(void)
 
     ui_RollerMinute = lv_roller_create(ui_PanelWidgets);
     lv_roller_set_options(ui_RollerMinute,
-                          "00\n01\n02\n03\n04\n05\n06\n07\n08\n09\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n21\n22\n23",
+                          "00\n01\n02\n03\n04\n05\n06\n07\n08\n09\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n21\n22\n23\n24\n25\n26\n27\n28\n29\n30\n31\n32\n33\n34\n35\n36\n37\n38\n39\n40\n41\n42\n43\n44\n45\n46\n47\n48\n49\n50\n51\n52\n53\n54\n55\n56\n57\n58\n59",
                           LV_ROLLER_MODE_INFINITE);
     lv_obj_set_height(ui_RollerMinute, 44);
     lv_obj_set_width(ui_RollerMinute, LV_SIZE_CONTENT);   /// 1
@@ -110,7 +110,7 @@ void ui_ScreenEpinkHome_screen_init(void)
 
     ui_RollerSecond = lv_roller_create(ui_PanelWidgets);
     lv_roller_set_options(ui_RollerSecond,
-                          "00\n01\n02\n03\n04\n05\n06\n07\n08\n09\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n21\n22\n23",
+                          "00\n01\n02\n03\n04\n05\n06\n07\n08\n09\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n21\n22\n23\n24\n25\n26\n27\n28\n29\n30\n31\n32\n33\n34\n35\n36\n37\n38\n39\n40\n41\n42\n43\n44\n45\n46\n47\n48\n49\n50\n51\n52\n53\n54\n55\n56\n57\n58\n59",
                           LV_ROLLER_MODE_INFINITE);
     lv_obj_set_height(ui_RollerSecond, 44);
     lv_obj_set_width(ui_RollerSecond, LV_SIZE_CONTENT);   /// 1
