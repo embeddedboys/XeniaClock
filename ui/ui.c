5215dda4e674941428f4180e4950a2a47bc9a8c6
IotaHydrae <writeforever@foxmail.com>
2023-01-12 18:41:49 +0800

    sync ui project
    Signed-off-by: IotaHydrae <writeforever@foxmail.com>

--- ui/ui.c
+++ ui/ui.c
@@ -59,7 +59,7 @@ void ui_ScreenEpinkHome_screen_init(void)
 
     ui_RollerHour = lv_roller_create(ui_PanelWidgets);
     lv_roller_set_options(ui_RollerHour,
-                          "00\n01\n02\n03\n04\n05\n06\n07\n08\n09\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n21\n22\n23\n24\n25\n26\n27\n28\n29\n30\n31\n32\n33\n34\n35\n36\n37\n38\n39\n40\n41\n42\n43\n44\n45\n46\n47\n48\n49\n50\n51\n52\n53\n54\n55\n56\n57\n58\n59",
+                          "00\n01\n02\n03\n04\n05\n06\n07\n08\n09\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n20\n21\n22\n23",
                           LV_ROLLER_MODE_NORMAL);
     lv_obj_set_height(ui_RollerHour, 46);
     lv_obj_set_width(ui_RollerHour, LV_SIZE_CONTENT);   /// 1
