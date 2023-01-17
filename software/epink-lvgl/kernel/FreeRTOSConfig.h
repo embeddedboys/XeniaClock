407ae84e69a610f911c524dc79e457aba0b5aa6b
IotaHydrae <writeforever@foxmail.com>
2023-01-17 16:58:37 +0800

    enable mutex support and tick hook
    Signed-off-by: IotaHydrae <writeforever@foxmail.com>

--- software/epink-lvgl/kernel/FreeRTOSConfig.h
+++ software/epink-lvgl/kernel/FreeRTOSConfig.h
@@ -18,7 +18,7 @@
 #define configIDLE_SHOULD_YIELD                 1
 #define configUSE_TASK_NOTIFICATIONS            1
 #define configTASK_NOTIFICATION_ARRAY_ENTRIES   3
-#define configUSE_MUTEXES                       0
+#define configUSE_MUTEXES                       1
 #define configUSE_RECURSIVE_MUTEXES             0
 #define configUSE_COUNTING_SEMAPHORES           0
 #define configQUEUE_REGISTRY_SIZE               10
@@ -37,7 +37,7 @@
 
 /* Hook function related definitions. */
 #define configUSE_IDLE_HOOK                     0
-#define configUSE_TICK_HOOK                     0
+#define configUSE_TICK_HOOK                     1
 #define configCHECK_FOR_STACK_OVERFLOW          0
 #define configUSE_MALLOC_FAILED_HOOK            0
 #define configUSE_DAEMON_TASK_STARTUP_HOOK      0
