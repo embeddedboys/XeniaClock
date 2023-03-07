35c7de4fc8b8bbf0e2c9479cff3105b49bcc9143
IotaHydrae <writeforever@foxmail.com>
2023-03-07 14:23:48 +0800

    add i2c scan
    Signed-off-by: IotaHydrae <writeforever@foxmail.com>

--- software/epink-lvgl/drivers/i2c/native_i2c.c
+++ software/epink-lvgl/drivers/i2c/native_i2c.c
@@ -30,17 +30,58 @@
 
 #include <common/init.h>
 #include <common/conf.h>
+#include <common/tools.h>
+#include <common/vals.h>
 
 #include "lib/printk.h"
 
 #include "i2c/native_i2c.h"
-#include "common/tools.h"
+
+#include "pico/stdlib.h"
+#include "pico/binary_info.h"
 #include "hardware/i2c.h"
 #include "hardware/gpio.h"
-#include "common/vals.h"
-#include "pico/binary_info.h"
 
 #define DEFAULT_I2C_IFCE        i2c0
+
+// I2C reserves some addresses for special purposes. We exclude these from the scan.
+// These are any addresses of the form 000 0xxx or 111 1xxx
+bool reserved_addr(uint8_t addr) {
+    return (addr & 0x78) == 0 || (addr & 0x78) == 0x78;
+}
+
+int i2c_bus_scan(i2c_inst_t *i2c)
+{
+    if (!i2c)
+        i2c = DEFAULT_I2C_IFCE;
+
+    printf("\nI2C Bus Scan\n");
+    printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
+
+    for (int addr = 0; addr < (1 << 7); ++addr) {
+        if (addr % 16 == 0) {
+            printf("%02x ", addr);
+        }
+
+        // Perform a 1-byte dummy read from the probe address. If a slave
+        // acknowledges this address, the function returns the number of bytes
+        // transferred. If the address byte is ignored, the function returns
+        // -1.
+
+        // Skip over any reserved addresses.
+        int ret;
+        uint8_t rxdata;
+        if (reserved_addr(addr))
+            ret = PICO_ERROR_GENERIC;
+        else
+            ret = i2c_read_blocking(i2c, addr, &rxdata, 1, false);
+
+        printf(ret < 0 ? "." : "@");
+        printf(addr % 16 == 15 ? "\n" : "  ");
+    }
+    printf("Done.\n");
+    return 0;
+}
 
 subsys_initcall(native_i2c_init);
 
